ec2dd51f80da8539f48ac8ed56a961d36a8de267
Renan Paiva <renan_13rj@yahoo.com.br>
2022-12-12 21:53:33 -0300

    Corrige logo

--- app.ts
+++ app.ts
@@ -1,16 +1,23 @@
 import AdminJS from 'adminjs';
 import adminJSExpress from '@adminjs/express';
 import  express  from "express";
+
 require('dotenv').config()
 
 const PORT = process.env.PORT_HOST;
 
 const start = async () => {
     const adminOptions = {
-        rootPath: '/admin'
+        rootPath: '/admin',
+        dashboard:{},
+        branding:{
+            logo: "/img/logo-make.svg", 
+            companyName: 'Make +'
+        }
     };
 
     const app = express();
+    app.use("/img", express.static("src/assets/img"));
     const admin = new AdminJS(adminOptions);
     const adminRouter = adminJSExpress.buildRouter(admin);
     app.use(admin.options.rootPath, adminRouter);
