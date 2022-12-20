19f319a4aec90072d9e46d89063a29358813d8c9
Renan Paiva <renan_13rj@yahoo.com.br>
2022-12-20 20:25:39 -0300

    Corrige type valor para decimal

--- src/model/service.entity.ts
+++ src/model/service.entity.ts
@@ -41,7 +41,7 @@ Service.init(
             }
         },
         valor: {
-            type: DataTypes.INTEGER,
+            type: DataTypes.DECIMAL(10,2),
             allowNull: false
         },
         createdAt: {
